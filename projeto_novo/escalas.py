0388086e7a53e8170c019da45d384e224020c631
Evandro Avellar <evandro.avellar@gmail.com>
2024-09-02 08:00:25 -0300

    Melhorando código e incluindo informações na docstrings

--- projeto_novo/escalas.py
+++ projeto_novo/escalas.py
@@ -1,22 +1,32 @@
 NOTAS = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
-INTERVALOS = [0, 2, 4, 5, 7, 9, 11]
+ESCALA = {'maior': [0, 2, 4, 5, 7, 9, 11]}
 
 
-def escalas(tonica, tonalidade):
+def escalas(tonica: str, tonalidade: str) -> dict[str, list[str]]:
     """
-    >>> escalas('C', 'maior')
-    {'notas': ['C', 'D', 'E', 'F', 'G', 'A', 'B'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
-    >>> escalas('A', 'maior')
-    {'notas': ['A', 'B', 'C#', 'D', 'E', 'F#', 'G#'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
+    Gerar uma escala a partir de uma tônica e uma tonalidade.
+
+    Parameters:
+        tonica: Nota que será a tônica da escala
+        tonalidade: Tonalidade da escala
+
+    Returns:
+        Um dicionário com as notas da escala e graus
+    
+    Examples:    
+        >>> escalas('C', 'maior')
+        {'notas': ['C', 'D', 'E', 'F', 'G', 'A', 'B'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
+
+        >>> escalas('A', 'maior')
+        {'notas': ['A', 'B', 'C#', 'D', 'E', 'F#', 'G#'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
     """
-    notas = []
     NOTAS_2 = [
         NOTAS[i % 12]
         for i in range(NOTAS.index(tonica), NOTAS.index(tonica) + 12)
     ]
 
-    for int in INTERVALOS:
-        notas.append(NOTAS_2[int])
+    notas = [NOTAS_2[int] for int in ESCALA[tonalidade]]
+
     return {
         'notas': notas,
         'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII'],
